00b41ac16c26bb0ac698601977c7f4a43eb04d43
Kevin Smith <kevinmilly@gmail.com>
2021-03-06 20:27:32 -0500

    added interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { ToolbarComponent } from './layout/toolbar/toolbar.component';
+import { TableComponent } from './presentational/table/table.component';
+import { GraphComponent } from './presentational/graph/graph.component';
+import { ButtonComponent } from './presentational/button/button.component';
+import { KanbanComponent } from './presentational/kanban/kanban.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ToolbarComponent,
+    TableComponent,
+    GraphComponent,
+    ButtonComponent,
+    KanbanComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
